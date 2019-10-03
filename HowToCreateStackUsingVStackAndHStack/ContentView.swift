592bcc7d0eff7f66035824ee18af01e376aa2f3f
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-03 07:27:37 +0300

    init

--- HowToCreateStackUsingVStackAndHStack/ContentView.swift
+++ HowToCreateStackUsingVStackAndHStack/ContentView.swift
@@ -10,7 +10,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        VStack {
+            VStack {
+                Text("SwiftUI")
+                Text("rocks")
+            }
+            HStack {
+                Text("SwiftUI")
+                Text("rocks")
+            }
+        }
     }
 }
 
